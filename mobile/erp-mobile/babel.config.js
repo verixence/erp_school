e06593f12416622a2b979f2b68c028a9f02075bd
Dhanush <96593764+dhanush-m@users.noreply.github.com>
2025-10-21 13:20:53 -0700

    Add react-native-worklets as devDependency for babel plugin
    - Babel preset requires worklets plugin when reanimated is present
    - Installing as devDependency for babel plugin only, not native code

--- mobile/erp-mobile/babel.config.js
+++ mobile/erp-mobile/babel.config.js
@@ -2,11 +2,16 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: [
-      'babel-preset-expo',
+      ['babel-preset-expo', { useTransformReactJSXExperimental: false }],
       'nativewind/babel',
     ],
     plugins: [
       'react-native-reanimated/plugin',
     ],
+    env: {
+      production: {
+        plugins: ['react-native-paper/babel'],
+      },
+    },
   };
 }; 
